eddd67f5a91e638338b3bf0165f48f6e2a63b291
vijaydbt40@gmail.com <vijaydbt40@gmail.com>
2022-06-17 05:00:35 +0000

    models example completed

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,11 @@
 select
-    orderid,
-    paymentmethod,
-    sum(amount)
-from
-    raw.stripe.payment
-where
-    status = 'success'
-group by
-    orderid,
-    paymentmethod
-order by
-    3 desc
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
